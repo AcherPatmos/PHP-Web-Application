2490e0826d5411cecbdd236cc4d652fa61df44fe
Patmos <a.mpakaniye@alustudent.com>
2026-08-14 20:20:03 +0400

    created the sql blue print for our heroes and users table

---
+++ Database file/heroes.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS hero_db;
+CREATE DATABASE hero_db;
+USE hero_db;
+
+CREATE table heroes(
+   hero_id  INT AUTO_INCREMENT PRIMARY KEY,
+   hero_name  VARCHAR(100)  NOT NULL,
+   real_name  VARCHAR(100)  NOT NULL,
+   short_bio  VARCHAR(255)  NOT NULL,   
+   long_bio   TEXT          NOT NULL,    
+  powers      VARCHAR(255)  NOT NULL,
+  team        VARCHAR(100)      NULL,
+  gender      VARCHAR(20)   NOT NULL,
+  status            ENUM('Active','Inactive','Deceased','Unknown')
+                                      NOT NULL DEFAULT 'Active',
+  created_at        TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at        TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+                                  ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE users (
+ 
+  user_id       INT AUTO_INCREMENT PRIMARY KEY,
+  username      VARCHAR(50)   NOT NULL UNIQUE,
+  password      VARCHAR(255)  NOT NULL,
+ 
+  full_name     VARCHAR(100)      NULL,
+  created_at    TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
